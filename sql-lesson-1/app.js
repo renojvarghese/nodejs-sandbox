6314039311cd51556cc38a399f9fc1470cb1c26b
Renoj Varghese <renoj.varghese@uconn.edu>
2018-04-30 21:43:50 -0400

    more sql

--- sql-lesson-1/app.js
+++ sql-lesson-1/app.js
@@ -1,16 +1,40 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
+const express = require('express');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "",
-  password: ""
+const app = express();
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "",
+    password: ""
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
+con.connect((err) => {
+    if (err) console.log(err);
+    else {
+        con.query("CREATE DATABASE IF NOT EXISTS text", (err, result) => {
+            if (err)
+            { console.log(err);
+            }
+            else {
+                console.log("DB created");
+
+            }
+            con.query("use text");
+            con.query("SELECT 1 FROM testTable LIMIT 1", (err, result) => {
+                if (err) {
+                    con.query("CREATE TABLE testTable (name VARCHAR(255), text VARCHAR(255))", (err, result) => {
+                        if (err) console.log(err);
+                        else {
+                            console.log(result);
+                        }
+                    })
+                }
+            })
+        });
+
+    }
+})
+
+app.get("/", (req, res) => {
+
 });
